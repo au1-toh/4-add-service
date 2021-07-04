f1269a4d13c3067ee35feb06be47db9cbeee42f1
David Kossoglyad (Hire me!) <david@logoui.co.il>
2021-07-05 02:43:58 +0300

    Initial commit

---
+++ src/app-components/index.ts
@@ -0,0 +1,8 @@
+import {FrameworkConfiguration, PLATFORM } from 'aurelia-framework';
+
+export function configure(config: FrameworkConfiguration): void {
+  config.globalResources([
+    PLATFORM.moduleName('./hero-details-component/hero-details-component.html'),
+    PLATFORM.moduleName('./heroes-component/heroes-component.html')    
+  ]);
+}
---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import { HEROES } from 'mock-heroes';
+import { Hero } from './hero';
+
+export class App {
+  
+  public title = 'Tour of Heroes';
+  public heroes: Hero[] = HEROES;
+
+}
